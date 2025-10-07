c91c8335c6bd9a5dffb58b585246ea01ee6c15f1
Ivan Trofimov <101204403+hanglider@users.noreply.github.com>
2025-10-07 11:04:32 +0300

    Create practic_5.R

---
+++ practic_5.R
@@ -0,0 +1,52 @@
+ad <- read.csv("/Users/ivan/IT/Mat_Statistic/Advertising.csv")
+l4 <- lm(Sales ~ TV + Radio + Newspaper, data = ad)
+summary(l4)
+
+l5 <- lm(Sales ~ TV + Radio, data = ad)
+summary(l5)
+
+l6 <- lm(ad$Sales ~ ad$TV + ad$Radio)
+summary(l6)
+predict(l6, newdata = data.frame(TV = 300, Radio = 50))
+predict(l6, newdata = data.frame(TV = 200, Radio = 100), interval = "p")
+#predict(l6, newdata = data.frame(TV = c(200, 100), Radio = c(100, 50)), interval = "prediction")
+
+# Проверка предположений регрессионного анализа
+plot(ad$Sales, l6$residuals)
+# Проверка на гомоскедастичность (тест Бройша-Пагана)
+install.packages("lmtest")
+library(LmTest)
+bptest(l6)
+
+# Проверка на автокорреляцию (тест Дарбина-Уотсона)
+dwtest(l6)
+
+# Проверка равенства нулю матожидания остатков 
+t.test(l6$residuals, mu = 0)
+
+# Проверка нормальности остатков
+shapiro.test(l6$residuals)
+
+# Средняя ошибка апрроксимации 
+mean(abs((ad$Sales - l6$fitted.values)) / ad$Sales)
+install.packages("Mlmetrics")
+library(MlMetrics)
+MAPE(ad$Sales, l6$fitted.values)
+
+# Мультиколлинеарность (зависимость между факториалами)
+install.packages("car")
+vif(l6)
+
+# Подбор лучшей модели (показатель Акаике)
+install.packages("MASS")
+library(MASS)
+stepAIC(l4)
+
+install.packages("leaps")
+library(Leaps)
+regsubsets(Sales ~ TV + Radio + Newspaper, data = ad, nvmax = 3)
+summary(regsubsets(Sales ~ TV + Radio + Newspaper, data = ad, nvmax = 3))$
+summary(regsubsets(Sales ~ TV + Radio + Newspaper, data = ad, nvmax = 3))$
+summary(regsubsets(Sales ~ TV + Radio + Newspaper, data = ad, nvmax = 2), scale = "adjr2")
+
+
